d0f0fe5789b8604991c10530d1b0e13f549f2346
TriscariGiorgio <giorgiotriscari@gmail.com>
2024-02-12 12:10:44 +0100

    Update creazioneDB.py

--- esempio6/creazioneDB.py
+++ esempio6/creazioneDB.py
@@ -1,10 +1,11 @@
 from funzioniDB import *
 import csv
+
 if __name__ == '__main__':
     pw = ""
     db = "museo"
     connection = create_server_connection("localhost", "root", pw)
-    esegui_query(connection,"DROP DATABASE museo")
+    esegui_query(connection, "DROP DATABASE museo")
     create_database(connection, "CREATE DATABASE museo")
     connection = create_db_connection("localhost", "root", pw, db)
     query = """SET GLOBAL max_allowed_packet = 268435456"""
@@ -18,30 +19,36 @@ if __name__ == '__main__':
         `nazionalita`  VARCHAR(255)
     );
     """)
-    esegui_query(connection,"""
+    esegui_query(connection, """
         CREATE TABLE IF NOT EXISTS `opera` (
         `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
         `titolo` VARCHAR(255),
         `thumbnail`  VARCHAR(255),
         `anno` int(11),
-        `id_artista` int(11)
+        `nome_artista` VARCHAR(255)
     );
     """)
     esegui_query(connection, """
     ALTER TABLE `opera` 
-      ADD CONSTRAINT `opera_ibfk_1` FOREIGN KEY (`id_artista`) REFERENCES `artista` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+      ADD CONSTRAINT `opera_ibfk_1` FOREIGN KEY (`nome_artista`) REFERENCES `artista` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
     """)
 
-    with open(r"C:\webApp\unione..csv",encoding="UTF-8") as f:
-        reader=csv.reader(f,delimiter=";")
+    with open(r"C:\webApp\unione..csv", encoding="UTF-8") as f:
+        reader = csv.reader(f, delimiter=";")
         next(reader)
-        reader=list(reader)
+        reader = list(reader)
     for x in reader[:2]:
         print(x)
 
-    lista_campi_artista=["nome","nazionalita"]
-    lista_artisti=[]
+    lista_campi_artista = ["name", "nazionalita"]
+    lista_artisti = []
     for i in range(len(reader)):
-        lista_artisti.append((reader[i][2],))
+        lista_artisti.append((reader[i][2], reader[i][5]))
 
-    inserisci_dati(connection,"artista",lista_campi_artista,lista_artisti)
+    lista_campi_opera=["titolo","thumbnail","anno","nome_artista"]
+    lista_opere=[]
+    for i in range(len(reader)):
+        lista_opere.append((reader[i][1], reader[i][4], reader[i][3],reader[i][2]))
+
+    inserisci_dati(connection, "artista", lista_artisti, lista_campi_artista)
+    inserisci_dati(connection, "opera", lista_opere, lista_campi_opera)
